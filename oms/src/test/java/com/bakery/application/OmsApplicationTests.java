f47374633c180b3d7480859269ffb039b9541805
liting <LiTing188225@yeah.net>
2019-05-01 21:38:42 +0800

    20190501

--- oms/src/test/java/com/bakery/application/OmsApplicationTests.java
+++ oms/src/test/java/com/bakery/application/OmsApplicationTests.java
@@ -1,6 +1,5 @@
 package com.bakery.application;
 
-import com.bakery.application.service.PersonService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +11,7 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OmsApplicationTests {
-    @Autowired
-    PersonService personService;
-    @Test
-    public void Test(){
-        List<person> p=personService.select();
-        System.out.println(p+"---");
-    }
+
 
 
 }
