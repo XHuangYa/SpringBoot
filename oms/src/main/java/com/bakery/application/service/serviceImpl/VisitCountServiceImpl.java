664e8a012eb49ef9a96eae54eea3cbfe08340c70
yangyukun@lt.com <yangyukun@lt.com>
2019-06-02 09:48:22 +0800

    提交

--- oms/src/main/java/com/bakery/application/service/serviceImpl/VisitCountServiceImpl.java
+++ oms/src/main/java/com/bakery/application/service/serviceImpl/VisitCountServiceImpl.java
@@ -7,7 +7,6 @@ package com.bakery.application.service.serviceImpl;
 
 import com.bakery.application.entity.VisitCount;
 import com.bakery.application.entity.VisitCountCriteria;
-import com.bakery.application.mapper.VisitCountMapper;
 import com.bakery.application.service.VisitCountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,15 +16,16 @@ import java.util.List;
 @Service
 @Transactional(readOnly = false)
 public class VisitCountServiceImpl implements VisitCountService {
-    @Autowired
-    VisitCountMapper visitCountMapper;
-    @Override
-    public boolean updateByExampleSelective(VisitCount visitCount, VisitCountCriteria countCriteria) {
-        return visitCountMapper.updateByExampleSelective(visitCount,countCriteria)>=1?true:false;
-    }
 
-    @Override
-    public List<VisitCount> selectByExample(VisitCountCriteria example) {
-        return visitCountMapper.selectByExample(example);
-    }
+	@Override
+	public boolean updateByExampleSelective(VisitCount record, VisitCountCriteria example) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<VisitCount> selectByExample(VisitCountCriteria example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
